4d849c607a33284beb2e2f9ae578dc050e8f78b2
Alessio Tucci <atucci@e3r3p7.42roma.it>
2023-01-17 14:37:22 +0100

    m|

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,8 +6,20 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 10:39:48 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 10:41:09 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/17 14:36:27 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_isalpha(int argum) // this is the first function to include in the library
+{
+	return ((argum >= 97 && argum <= 128) || (argum >= 65 && argum <= 90));
+}
 
+#include <stdio.h> // including this library for testing purpose 
+int	main()
+{
+	char num;
+
+	num = '0';
+	printf( "%d\n", ft_isalpha(num));
+}
