caf2f44a6500c628f2b71c030b675ebe47f5856b
Alessio Tucci <atucci@e3r4p2.42roma.it>
2023-01-18 14:24:21 +0100

    to  lower

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,8 +6,28 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:02:52 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:02:56 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/18 14:23:56 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// the tolower functions converts a uppercase alphabet to a lowercase alphabet
+int	ft_tolower(int chad)
+{
+	if (chad >= 65 && chad <= 90)
+		chad += 32;
+	return (chad);
+}
+// including main for testing purpose
+#include <unistd.h>
 
+int	main()
+{
+	char check;
+ // I decided to try to use the write function without any luck
+	check = 'G';
+	write(1, &check, 1);
+	ft_tolower(check);
+	write(1, "\n" , 1);
+	write(1, ft_tolower(check), 1);
+	//printf( "%d\n", ft_tolower(check));
+}
