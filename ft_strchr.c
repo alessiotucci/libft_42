69302d4eccd754e70cadcade0716449f10af18eb
Alessio Tucci <atucci@e3r4p2.42roma.it>
2023-01-18 16:25:10 +0100

    fixed the issues

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,21 +6,19 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:03:09 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/18 15:23:56 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/18 16:24:21 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // The C library function char *strchr(const char *str, int c) searches for the first occurrence of the character c (an unsigned char) in the string pointed to by the argument str.
-char *ft_strchr(char c,const char *str)
+char *ft_strchr(int d,const char *str)
 {
-	int count;
 
-	count = 0;
-	while (str[count])
+	while (*str != '\0')
 	{
-		if (str[count] == c)
-			return (&str[count]);
-	count++;
+		if (*str == d)
+			return (char *) str;
+	str++;
 	}
 	return (0);
 }
