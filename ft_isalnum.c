05605c05497d2c112cf0fc265511ae7f33ab473f
Alessio Tucci <atucci@e3r4p6.42roma.it>
2023-01-18 10:50:51 +0100

    m

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,12 +6,15 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:59:33 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 14:53:41 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/18 10:50:24 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // in this function I can use the two previous one together...
-int isalnum(int argum)
-{
+// to do so I need to include my custom made Library...
 
+#include "libft.h"
+int ft_isalnum(int argum)
+{
+	return ((ft_isalpha(argum) || ft_isdigit(argum)); 
 }
