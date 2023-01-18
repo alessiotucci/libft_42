b18eb79ceabf79309420eabf8b95a9beb70d483d
Alessio Tucci <atucci@e3r4p2.42roma.it>
2023-01-18 14:06:54 +0100

    adding  toupper

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,15 +6,23 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:02:34 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/18 11:43:27 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/18 14:06:31 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // The toupper() function converts a lowercase alphabet to an uppercase alphabet.
-int	toupper(int chad)
+int	ft_toupper(int chad)
 {
 	if (chad >= 97 && chad <= 122)
-		chad -= 27;
+		chad -= 32;
 	return (chad);
 }
+// including main to test the function 
+#include <stdio.h> 
+int	main()
+{
+	char ciao;
 
+	ciao = '+';
+	printf( "%d\n", ft_toupper(ciao));
+}
