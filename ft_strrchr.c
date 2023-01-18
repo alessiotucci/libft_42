7b103214754e90316ee04ac3cb146bcbe3755553
Alessio Tucci <atucci@e3r4p2.42roma.it>
2023-01-18 16:07:31 +0100

    adding strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,8 +6,10 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:03:23 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:03:27 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/18 15:27:52 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+/The strrchr() function finds the last occurrence of c (converted to a character) in string . The ending null character is considered part of the string . The strrchr() function returns a pointer to the last occurrence of c in string .
+/*
