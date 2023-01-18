66e9faa27504ce3ee68610a0f49fc9d746069261
Alessio Tucci <atucci@e3r4p6.42roma.it>
2023-01-18 11:02:55 +0100

    18 gennaio

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,8 +6,13 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:59:50 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 11:59:54 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/18 11:02:28 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+ // The isascii() function tests if a given character, in the current locale, can be represented as a valid 7–bit US-ASCII character.
 
+int	ft_isascii(argum)
+{
+	return (argum >= 0 && argum <= 127);
+}
