1d92c6f5f690ee8038de4155ae3b925f82d1b5c3
Alessio Tucci <atucci@e3r4p6.42roma.it>
2023-01-18 11:43:58 +0100

    inizio toupper

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,8 +6,15 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:02:34 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:02:38 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/18 11:43:27 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// The toupper() function converts a lowercase alphabet to an uppercase alphabet.
+int	toupper(int chad)
+{
+	if (chad >= 97 && chad <= 122)
+		chad -= 27;
+	return (chad);
+}
 
