5cc427899a92e4614cc26521d4e876b97d864242
Alessio Tucci <atucci@e3r3p7.42roma.it>
2023-01-19 11:04:13 +0100

    fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,8 +6,27 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:02:18 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:02:24 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/19 11:03:29 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+size_t	ft_strlcat(char *dest, const char *sour, size_t destsize)
+{
+	size_t cu; // the difference here is in the datatype, instead of unsigned int, you need to use size_t
+	size_t di;
+
+	if (destsize <= ft_strlen(dest))
+		return (destsize + ft_strlen(sour));
+	cu = ft_strlen(dest);
+	di = 0;
+	while (src[di] != '\0'&& cu + 1 < destsize)
+	{
+		dest[cu] = sour[di];
+		cu++;
+		di++;
+	}
+	dest[cu] = '\0';
+	return (ft_strlen(dest) + ft_strlen(&src[di]));
+}
