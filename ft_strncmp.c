aa5f4b29b8ab8816319e26412238d53c0867ad7c
Alessio Tucci <atucci@e3r3p7.42roma.it>
2023-01-19 11:43:58 +0100

    adding strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,8 +6,37 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:03:43 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:03:46 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/19 11:43:27 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// i need  to  include stddef.h to use  size_t
 
+#include <stddef.h> 
+
+size_t	ft_strncmp(const char *str1, const char *str2, size_t limit)
+{
+	size_t count;
+
+	count = 0;
+	while ((str1[count] != '\0' || str2[count] != '\0')
+		&& count < limit)
+	{
+		if (str1[count] > str2[count])
+		return (1);
+		else  if (str1[count] < str2[count])
+		return (-1);
+	count++;
+	}
+	return (0);
+}
+// including main for testing  purpose
+#include <stdio.h>
+
+int	main()
+{
+	int my_limit = 2;
+	char st[] = "sei scarso";
+	char ss[] = "sei marso";
+	printf( "%zu\n", ft_strncmp(st, ss,  my_limit));
+}	
