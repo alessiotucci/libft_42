0830e28d95fb3ea4842358d1b77595a5ec3c60b5
Alessio Tucci <atucci@e3r3p7.42roma.it>
2023-01-20 12:20:22 +0100

    adding small things

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:03:23 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/19 10:42:05 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/20 12:19:56 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,31 @@
 /*The strrchr() function finds the last occurrence of c (converted to a character) in string . The ending null character is considered part of the string . The strrchr() function returns a pointer to the last occurrence of c in string .
 */ 
 #include <string.h>
-char	*ft_strrchr(int d, const char *str)
+ char	*ft_strrchr(int d, const char *str)
 {
 	int len;
+	const char *last;
 
+	last = 0;
 	len = strlen(str);
 	while (len >= 0)
 	{
 		if (str[len] == d)
-			return (str[len]);
+		{
+			last = &str[len];
+			return (char *) last;
+		}
 		len--;
 	}
-	return (0);
+	return (char *)last;
 }
 
 // including main for testing purpose
 #include <stdio.h>
 int	main()
 {
-	char tick = 'h';
+	char tick = 'z';
 
-	char *brosk = "ciao hello hai mangiato hallowen";
+	char *brosk = "ciao hello hai mangiato halloween";
 	printf( "%s\n", ft_strrchr(tick, brosk));
 }
