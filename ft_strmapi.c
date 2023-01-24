77aefef38795534595969c1c1499ae58ff05aa80
Alessio Tucci <atucci@e3r9p4.42roma.it>
2023-01-24 17:24:55 +0100

    adding stuff

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,8 +6,51 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:10:59 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:11:01 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/24 17:24:28 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+char    to_upper(unsigned int i, char c)
+{
+    (void)i;
+    return (c >= 'a' && c <= 'z' ? c - 32 : c);
+}
+char	*ft_strmapi(char const *s, char(*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char	*new;
 
+	if (!s || !f) // check if the input are valid
+	return (0);
 
+	new = (char *)malloc(sizeof(char) * (strlen(s) + 1));
+	
+	if (new == 0) // check if the memory allocation 
+		return (0);
+
+	i = 0;
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+int main()
+{
+    char *s = "HELLO wolrslf";
+    char *new_str;
+
+    /* Pass the input string and the tolower function as arguments to ft_strmapi */
+    new_str = ft_strmapi(s, to_upper);
+
+    /* Print the result of the function */
+    printf("Original string: %s\n", s);
+    printf("New string: %s\n", new_str);
+
+    
+}
