8c9f7b3c43afdc7e306d4fcef6723131b13c7e54
Alessio Tucci <atucci@e3r4p4.42roma.it>
2023-01-24 16:21:54 +0100

    itoa and split, split doesnt work yet

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,8 +6,62 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:10:24 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:10:26 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/24 16:21:22 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+int	digits(int n)
+{
+	int count = 0;
+	count = (n <= 0) ? 1 : 0;
 
+// (n) is less than or equal to zero. If it is, then the code is setting the length (len) to be 1. 
+
+//If it's not, then the code is setting the length (len) to be 0.
+	while (n != 0) 
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	int	len;
+	char	*res;
+
+	if (n == -214783648) // special case for the lowest int value
+		return ("-214783648");
+	len = digits(n);
+	res = (char *)malloc((len + 1) * sizeof(char));
+	if (res == 0) // checking the memory allocation
+		return (0);
+
+	if (n < 0) // negative number, handling the minus sign
+	{
+		res[0] = '-'; // minus on before the first digit
+		res[len--] = '\0'; 
+		n = -n; // make this  positive after handling the  sign
+	}
+	else
+		res[len--] = '\0'; // starting from right to left
+
+	if (n == 0) // zero number, single digits
+	{
+		res[0] = '0';
+	}
+	while (n > 0) // positive number, easy work
+	{
+	res[len--] = (n % 10) + '0';
+	n /= 10;
+	}
+	return (res);
+}
+#include <stdio.h>
+int main()
+{
+	int n = 45;
+	printf( "%s\n",  ft_itoa(n));
+}
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,96 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:10:04 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:10:13 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/24 15:13:11 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+char	**ft_split(char const *s, char c)
+{
+// how  many strings do I need  to allocate (1);
+	int len;
+	len = strlen(s);
+	int *count =  0;
+
+	int i; // counter  to iterate  throught the string
+	i = 0;
+	while (i < len)
+	{
+		while (i < len)
+		{
+			if (strchr(&c,s[i]) == 0) // return is == there is not corrispondence with the separator char
+				break;
+			i++;
+		}
+		
+		int old;	//old is an other counter, to check is increment the substring count only if we have the need to do so.
+		old = i;
+		while (i < len)
+		{
+			if (strchr(&c,s[i]  != 0)) // return is != there IS a corrispondence with the separator char
+				break;
+			i++;
+		}
+	if (i > old) 
+		*count = *count + 1; // this is the check to do.
+	}
+	// now we can use Malloc to allocate the right amount of space for the string;
+	char **strings = malloc(sizeof(char *) * *count);
+
+	i = 0; // setting i = 0 because we are starting again from index zero;
+	
+	int index; 
+	index = 0;
+	
+	char buffer [16384]; // we are creating a buffer to store  the strings
+
+	while (i < len)
+
+	{
+		while (i < len)
+		{
+			if (strchr(&c,s[i]) == 0) // same  thing as before, == look it up
+				break;
+			i++;
+		}
+		
+		int j = 0;	
+		while (i < len)
+		{
+			if (strchr(&c,s[i]  != 0)) 
+				break;
+			buffer[j] = s[i];
+			j++;
+			i++;
+		}
+		if (j > 0)
+		{
+			buffer[j] = '\0';
+		
+			int alloca = sizeof(char) * (strlen(buffer) + 1);
+
+			strings[index] = malloc(alloca);
+			strcpy(strings[index], buffer);
+			index++;
+		}
+		
+
+	}
+return (strings);
+}
+
+int main()
+{
+	char prova[] = "	tu	io	ci	";
+	char t = '\t';
+	char **splitted = ft_split(prova,t);
+	
+	// for loops are forbidden but I use it for testing purpose
+	for (int i = 0; i < 4; i++)
+		printf( "%s\n", splitted[i]);
+}
 
