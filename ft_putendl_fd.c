1818fb5240f6522d664051bd3267497d43f8a42a
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-25 12:06:41 +0100

    adding putendline

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,8 +6,42 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:12:54 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:12:56 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/25 12:05:42 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stddef.h>
+#include <unistd.h>
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+	// &c work like a regular putchar function
+	// fd is the new paramater we pass inside write function
+}
 
+void	ft_putstr_fd(char *c, int fd)
+{
+	size_t count = 0;
+	if (c != 0 ) // checking the input
+	{
+		while (c[count])
+		{
+		ft_putchar_fd(c[count], fd);
+		count++;
+		}
+	}
+	// &c work like a regular putchar function
+	// fd is the new paramater we pass inside write function
+}
 
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd( '\n',fd);
+}
+
+int main()
+{
+	char stringa[] = "ciao come stai";
+	ft_putendl_fd(stringa, 1);
+	write(1, "%", 1);
+}
