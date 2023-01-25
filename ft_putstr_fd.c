c0cfb8d0760004f8a13ee8b7015f9201d58b40c1
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-25 11:43:46 +0100

    adding others

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,8 +6,40 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:12:34 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/17 12:12:39 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/25 11:43:31 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+#include <unistd.h>
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+	// &c work like a regular putchar function
+	// fd is the new paramater we pass inside write function
+}
 
+void	ft_putstr_fd(char *c, int fd)
+{
+	size_t count = 0;
+	if (c != 0 ) // checking the input
+	{
+		while (c[count])
+		{
+		ft_putchar_fd(c[count], fd);
+		count++;
+		}
+	}
+	// &c work like a regular putchar function
+	// fd is the new paramater we pass inside write function
+}
+
+int	main()
+{
+	char cic[] = "yall aint shit";
+	int	pob = 1;
+	ft_putstr_fd(cic, pob);
+	write(1, "\n", 1);
+	write(1, "f", 1);
+
+}
