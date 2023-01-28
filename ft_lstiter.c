36082f87e6a95dff6d4e4eef61a34ff7e2d9f9fb
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-28 13:34:49 +0100

    m:

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:44:18 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/28 13:30:48 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 13:34:37 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,4 @@ void	ft_lstiter(t_list *lst, void (*f) (void *))
 		f(lst->content); // applying the f  function to the  current  node;
 		lst = lst->next; // updating  the node of the list;
 	}
-}
 }
