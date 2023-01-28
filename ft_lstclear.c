79ca837ff7f640beccfa63c674f6b256a98be825
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-28 13:21:10 +0100

    adding studd idk

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,11 +6,21 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:42:57 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/25 16:43:54 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 13:20:45 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list *temp; // temporary variable;
 
+	if (*lst == NULL)
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del); // recalling the function to delete a single node.
+		*lst = temp;
+	}
+	*lst = 0 
 }
