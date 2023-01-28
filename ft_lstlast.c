8581db34f263447a3039eb552b906ecf79276ba2
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-28 11:07:47 +0100

    adding list

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,11 +6,20 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:38:50 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/25 16:39:48 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 11:07:30 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 t_list	*ft_lstlast(t_list *lst)
 {
-
+	// check the  list first
+	if (lst == NULL)
+		return (0);
+	
+	// this function works like ft_list_size, it just  return a Node
+	while (lst->next != NULL)
+	{
+	lst = lst -> next; // updating the pointer; 
+	}
+	return (lst);
 }
