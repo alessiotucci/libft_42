2f44c55f7bf6bd69e9622a8f891af43fb6a83ca4
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-28 12:12:31 +0100

    adding deletinf one elem

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,11 +6,14 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:42:01 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/25 16:42:47 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 12:11:58 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-
+	if (lst == NULL)
+		return ; // first check if the node exist; if null, return
+	del(lst->content); // recalling the function
+	free(lst); // freeing the node 
 }
