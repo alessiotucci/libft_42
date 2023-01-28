f53b86e91ff7ea7e1193f354f3c1a33e9ec29aae
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-28 10:43:36 +0100

    adding lenght  of the list

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,12 +6,20 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:37:51 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/25 16:38:26 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 10:43:03 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_lstsize(t_list *lst)
 {
+	int count;
 
+	count = 0;
+	while (lst != NULL)
+	{
+	count++; // increment the counter 
+	lst = lst -> next; // updating the pointer; 
+	}
+	return (count);
 }
 
