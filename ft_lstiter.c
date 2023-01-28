4a5cd6fb2015e4380ce08e7ef8d9e8b65e3e3823
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-28 13:31:05 +0100

    adding sstudf

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,11 +6,18 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:44:18 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/25 16:45:02 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 13:30:48 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_lstiter(t_list *lst, void (*f) (void *))
 {
-
+	if (lst == NULL || f == NULL) // checking if the  input is valid
+		return ;
+	while (lst != NULL) // iterate  throught  the  lenght of the list
+	{
+		f(lst->content); // applying the f  function to the  current  node;
+		lst = lst->next; // updating  the node of the list;
+	}
+}
 }
