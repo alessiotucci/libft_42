7f39a45f9a014cb4d9ba14b6a59e69f263423428
Alessio Tucci <atucci@e3r4p8.42roma.it>
2023-01-28 11:32:17 +0100

    adding list stuff

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:40:01 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/28 10:32:44 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 11:32:00 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	// check the input;
 	if (new = NULL)
 		return ;
-	if (*lst = NULL)
+	if (*lst = NULL) // if the list is empty
+		*lst = new; // update the  list and  add  it to the first  and last place :)
 		return ;
 	// after this check we can procede to update the last node of the list;
+	// to do so, we can use the ft_listLast function
+	temp = ft_lstlast(*lst);
+	temp->next = new;
+	// temp has the same values as the last  node, and it updates the pointer  to point  to the new node 
+	
 }
