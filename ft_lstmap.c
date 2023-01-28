8f8044079e3017f237c8031cf547f09fca82b6ba
Alessio Tucci <atucci@e3r9p3.42roma.it>
2023-01-28 15:36:02 +0100

    adding stuff

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:45:22 by atucci            #+#    #+#             */
-/*   Updated: 2023/01/28 15:27:29 by atucci           ###   ########.fr       */
+/*   Updated: 2023/01/28 15:35:43 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		}
 		ft_lstadd_back(&new_lst, ely);
 		lst = lst-next;
-}
+	}
 	return (new_lst); // returning the  newly created list
 }
